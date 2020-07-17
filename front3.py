c1eec2211110a7ba7fbcabb03e432cada924bc85
Arya <arya.mane03@gmail.com>
2020-07-17 16:12:35 -0400

    Create front3.py

---
+++ front3.py
@@ -0,0 +1,16 @@
+def front3(str):
+  substr = str[:3]
+  return substr+substr+substr
+
+
+print(front3("afdsfdf"))
+print(front3("a"))
+
+# their
+# def front3(str):
+#   # Figure the end of the front
+#   front_end = 3
+#   if len(str) < front_end:
+#     front_end = len(str)
+#   front = str[:front_end]
+#   return front + front + front 
